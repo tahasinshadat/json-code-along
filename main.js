293b74edb30b297611ee2d91249fadd6a814419e
ChaoticTeal <jawhittom@gmail.com>
2022-08-03 00:13:32 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+/** @TODO - Access the pokemon data from data.js */
+/**         and print to the console to check */
+
+
+/** @TODO - Update the page data using a single pokemon */
+/**  
+ *      1. Update the #poke_name heading with the name and dex number
+ *      2. Update the #poke_img with the pokemon's image URL
+ *      3. Update the type heading with the pokemon's type(s)
+ *      4. Change the width of each stat bar using the 
+ *          pokemon's base stats
+ */
+
+
+/** @TODO - Assign #random_btn to pick a random starter on click */
+/**         and update the page data accordingly
+ * 
+ *      1. Make a new function for the code from the previous task
+ *      2. Generate a random index for the pokemon array
+ *      3. Replace references to the specific pokemon with references
+ *          to the randomly-chosen pokemon
+ *      4. Add an eventListener to #random_btn with our new function!
+ */
